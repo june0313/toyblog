232772bdb57713d04d6c51c4090b68f5e489a77f
Young Jun Park <june0313@gmail.com>
2017-12-09 23:42:39 +0900

    초기 개발환경 설정

---
+++ src/main/java/net/jun/toyblog/ToyBlogApplication.java
@@ -0,0 +1,12 @@
+package net.jun.toyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyBlogApplication.class, args);
+	}
+}
